ebf49707fd3900474d4605f7adaf118bdd72cc10
Kirill Skorobogatov <Kirill9806@gmail.com>
2018-09-14 11:54:47 +0300

    Update Answer.py

--- Answer.py
+++ Answer.py
@@ -1,3 +1,5 @@
+import time
+
 a = {0: 'ноль', 
      1: 'один', 
      2: 'два', 
@@ -13,7 +15,13 @@ class RangeException(Exception):
   pass
 
 def firsttaskfivesem(x, y=''):
+  """
 
+  """
+  start_time = time.ctime(time.time())
+  logger = []
+  logger.append("Время и дата запуска функции: " + start_time)
+  
   b = {'bin': bin(x),
        'oct': oct(x),
        'hex': hex(x)}
@@ -23,11 +31,22 @@ def firsttaskfivesem(x, y=''):
   if x in a.keys():
     result.clear()
     result.append(a.get(x))
-
     if y in b.keys():
         result.append(b.get(y))
+    elif y != '':
+        print('Выбирите систему счисления: hex, bin или oct!')
+        logger.append("Исключения: ConvertTypeException")
+      
   else:
-    raise RangeException('Введите число от 0 до 9!')
+#    raise RangeException('Введите число от 0 до 9!')
+     print('Введите число от 0 до 9!')
+     logger.append("Исключения: RangeException")
+
+  if len(logger) == 1:   
+     logger.append("Исключения: не выявлено!")
+
+  with open('newfile.txt', 'a') as f:
+   f.write(str(logger) + "\n")
   return result
 
 def tests():
@@ -37,5 +56,5 @@ def tests():
 if __name__ == "__main__":
   x = int(input('Введите первый аргумент:'))
   y = input('Введите второй аргумент(а можете и не вводить):')
-  tests()
+ # tests()
   print(firsttaskfivesem(x,y))
